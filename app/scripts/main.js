60870770acc46f63b9dabd53f5e0f05634d55647
Ryan Murphy <github@accounts.rdmurphy.org>
2015-04-07 16:04:52 -0500

    New things

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -8,30 +8,8 @@
   map.scrollWheelZoom.disable();
   var layer = L.mapbox.featureLayer().addTo(map);
 
-  var tbody = shoestring('#tbody');
   var agendaEntries = shoestring('#agenda-entries');
-
-  shoestring.get('assets/data/tracker.json', function(res) {
-    var data = JSON.parse(res).reverse();
-    var tableBody = '';
-
-    for (var i = 0, len = data.length; i < len; i++) {
-      if (data[i].lat && data[i].lon) {
-        var marker = L.marker([data[i].lat, data[i].lon]);
-        marker.bindPopup('<strong>' + data[i].location + '</strong><br>' + data[i].venue + '<br>' + data[i].date);
-        layer.addLayer(marker);
-      }
-
-      tableBody += '<tr>' +
-        '<td data-title="Date"><p>' + data[i].date + '</p></td>' +
-        '<td data-title="Venue"><p>' + data[i].venue + '</p></td>' +
-        '<td data-title="Location"><p>' + data[i].location + '</p></td>' +
-        '<td data-title="Description">' + data[i].description_md + '</td>';
-    }
-
-    tbody.html(tableBody);
-    map.fitBounds(layer.getBounds());
-  });
+  var trackerEntries = shoestring('#tracker-entries');
 
   shoestring.get('assets/data/agenda.json', function(res) {
     var data = JSON.parse(res);
@@ -40,8 +18,8 @@
     for (var i = 0, len = data.length; i < len; i++) {
       entries += '<div class="entry">';
       entries += '<ul class="date">';
-      entries += data[i].date ? '<li>' + data[i].date + '</li>' : '';
-      entries += data[i].venue ? '<li>' + data[i].venue + '</li>' : '';
+      entries += '<li>' + (data[i].date || '&nbsp;') + '</li>';
+      entries += '<li>' + (data[i].venue || '&nbsp;') + '</li>';
       entries += data[i].location ? '<li><span class="icon-map-marker"></span> ' + data[i].location + '</li>' : '';
       entries += '</ul>';
 
@@ -53,5 +31,28 @@
     }
 
     agendaEntries.html(entries);
+  });
+
+  shoestring.get('assets/data/tracker.json', function(res) {
+    var data = JSON.parse(res).reverse();
+    var entries = '';
+
+    for (var i = 0, len = data.length; i < len; i++) {
+      entries += '<div class="entry">';
+      entries += '<ul class="date">';
+      entries += data[i].date ? '<li>' + data[i].date + '</li>' : '';
+      entries += data[i].venue ? '<li>' + data[i].venue + '</li>' : '';
+      entries += data[i].location ? '<li><span class="icon-map-marker"></span> ' + data[i].location + '</li>' : '';
+      entries += '</ul>';
+
+      entries += '<ul class="info">';
+      entries += '<li>' + data[i].description_md + '</li>';
+      entries += data[i].host ? '<li class="host"><strong>Host</strong>: ' + data[i].host + '</li>' : '';
+      entries += '</ul>';
+      entries += '</div>';
+    }
+
+    trackerEntries.html(entries);
+    map.fitBounds(layer.getBounds());
   });
 })();
